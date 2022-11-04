d3ae0253f8de34ac2cf77264ca962df76c202bca
DimaPosokhov <dima.posokhov.98@gmail.com>
2022-11-04 16:05:25 +0200

    task 3 is ready

---
+++ Task_3/Employee.cs
@@ -0,0 +1,65 @@
+﻿namespace Task_3
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public readonly double PDFOCoef = 0.18;
+        public enum Post
+        {
+            Engineer = 10000,
+            Janitor = 5000,
+            QA = 7500,
+            Manager = 9000
+        }
+
+        public double salaryCalculations(int workExperience, Post post)
+        {
+            if (workExperience > 20)
+            {
+                switch (post)
+                {
+                    case Post.Engineer:
+                        return (double)post * 2.5;
+                    case Post.Janitor:
+                        return (double)post * 2.2;
+                    case Post.QA:
+                        return (double)post * 2.25;
+                    case Post.Manager:
+                        return (double)post * 2.1;
+                    default:
+                        Console.WriteLine("This post doesn't exist in our company");
+                        break;
+                }
+
+            }
+            else if (workExperience > 10)
+            {
+                switch (post)
+                {
+                    case Post.Engineer:
+                        return (double)post * 1.75;
+                    case Post.Janitor:
+                        return (double)post * 1.7;
+                    case Post.QA:
+                        return (double)post * 1.7;
+                    case Post.Manager:
+                        return (double)post * 1.65;
+                    default:
+                        Console.WriteLine("This post doesn't exist in our company");
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("Your work experience is too short");
+            return (double)post;
+        }
+        public Employee(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+
+
+}   
---
+++ Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Task_3;
+
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
+Employee employee = new(firstName, lastName);
+
+
+Console.WriteLine("Choose your post in a company:\n" +
+                  "1 - Engineer\n" +
+                  "2 - Janitor\n" +
+                  "3 - QA\n" +
+                  "4 - Manager\n");
+
+string choice = Console.ReadLine();
+Employee.Post post = 0;
+if (choice == "1")
+    post = Employee.Post.Engineer;
+else if (choice == "2")
+    post = Employee.Post.Janitor;
+else if (choice == "3")
+    post = Employee.Post.QA;
+else if (choice == "4")
+    post = Employee.Post.Manager;
+else
+    Console.WriteLine("Wrong choice! This post doesn't exist in our company");
+
+
+Console.WriteLine("Enter your work experience: ");
+int workExperience = Convert.ToInt32(Console.ReadLine());
+double salary = employee.salaryCalculations(workExperience, post);
+Console.WriteLine($"Your name: {firstName} {lastName}.\nYour post: {post}.\nYour salary: {salary}.\nYour PDFO from salary: {salary * employee.PDFOCoef}");
+
